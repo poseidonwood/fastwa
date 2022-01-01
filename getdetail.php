12c015f8a2d327dd1cab5a382eba70c3969f8b29
Poseidonwood <santosofebrikukuh@gmail.com>
2022-01-01 22:47:31 +0700

    auto commit

--- getdetail.php
+++ getdetail.php
@@ -1,22 +1,23 @@
 <?php
 include_once ("config.php");
 if(is_array($port)){
-  echo "here"; exit;
+  foreach ($data as $datanya) {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "http://fekusa.xyz:{$datanya}/getdetail",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
+  }
 }
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'http://fekusa.xyz:4000/getdetail',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
---
+++ run.bash
@@ -0,0 +1,6 @@
+git config --global user.email "santosofebrikukuh@gmail.com"
+git config --global user.name "Poseidonwood"
+git add .
+git commit -m 'auto commit'
+echo "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"
+git push
